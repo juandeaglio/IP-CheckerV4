92679d3dca674e6cb560208a908eaaa0f32d8f31
juandeaglio <45726108+juandeaglio@users.noreply.github.com>
2020-11-24 14:08:06 -0800

    Introduced VPN Stability Monitor functionality
    WIP.

--- IP Checker/VPN Stability Monitor.cs
+++ IP Checker/VPN Stability Monitor.cs
@@ -6,7 +6,41 @@ using System.Threading.Tasks;
 
 namespace IP_Checker
 {
-    class VPN_Stability_Monitor
+    static class VPN_Stability_Monitor
     {
+        private static int totalWarnings;
+        private static int threshold = 5;
+        private static bool thresholdReached = false;
+        private static string HomeIP { get; set; }
+        private static string VPNIP { get; set; }
+        static VPN_Stability_Monitor()
+        {
+            
+        }
+        private static int ShutdownThresholdCounter
+        {
+            get => totalWarnings;
+            set
+            {
+                //Do something if thresholdReached.
+                if (totalWarnings > threshold && !thresholdReached)
+                    thresholdReached = true;
+            }
+        }
+        static void StabilityCheck()
+        {
+            if(!IPMonitor.currentIP.Equals(HomeIP) && IsValidIP(IPMonitor.currentIP))
+            {
+
+            }
+        }
+        public static void Run()
+        {
+
+        }
+        static bool IsValidIP(string ip)
+        {
+            return true;
+        }
     }
 }
