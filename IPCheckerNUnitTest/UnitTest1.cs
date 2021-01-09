26b2b98c91deedc5071a74f2f6188a6e3d842f18
juandeaglio <45726108+juandeaglio@users.noreply.github.com>
2021-01-08 21:35:37 -0800

    Added two approval tests.

--- IPCheckerNUnitTest/UnitTest1.cs
+++ IPCheckerNUnitTest/UnitTest1.cs
@@ -7,10 +7,23 @@ namespace IPCheckerNUnitTest
         [SetUp]
         public void Setup()
         {
+            IPMonitor.UpdateWebsitesAction += (websites) => { };
+        }
+        [Test]
+        public void ShouldAddWebsite()
+        {
+            IPMonitor.AddWebsite("http://icanhazip.com");
+            foreach(string website in IPMonitor.websites)
+            {
+                Assert.IsNotEmpty(website);
+            }
+            
         }
         [Test]
         public void ShouldHaveConnectionTrue()
         {
+
+            IPMonitor.AddWebsite("http://icanhazip.com");
             Assert.IsTrue(IPMonitor.IsConnectionActive());
         }
     }
