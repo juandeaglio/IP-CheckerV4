6dac05f482d71d44ec105c512301f3eb11f91f35
juandeaglio <45726108+juandeaglio@users.noreply.github.com>
2021-01-08 22:17:20 -0800

    Added TearDown and Setup functions. Added more tests.

--- IPCheckerNUnitTest/UnitTest1.cs
+++ IPCheckerNUnitTest/UnitTest1.cs
@@ -1,9 +1,18 @@
 using NUnit.Framework;
 using IP_Checker;
+using System;
 namespace IPCheckerNUnitTest
 {
     public class Tests
     {
+        public const string WEBSITE1 = "http://icanhazip.com";
+        public const string WEBSITE2 = "http://checkip.dyndns.org/";
+        private const string WEBSITE3 = "http://ifconfig.me/ip";
+        [TearDown]
+        public void TearDown()
+        {
+            IPMonitor.websites.Clear();
+        }
         [SetUp]
         public void Setup()
         {
@@ -12,19 +21,50 @@ namespace IPCheckerNUnitTest
         [Test]
         public void ShouldAddWebsite()
         {
-            IPMonitor.AddWebsite("http://icanhazip.com");
-            foreach(string website in IPMonitor.websites)
-            {
-                Assert.IsNotEmpty(website);
-            }
-            
+            IPMonitor.AddWebsite(WEBSITE1);
+            Assert.IsTrue(WebsitesAreNotEmpty(IPMonitor.websites));
+            Assert.GreaterOrEqual(IPMonitor.websites.Count, 1);
+        }
+        [Test]
+        public void ShouldRemoveWebsite()
+        {
+            IPMonitor.AddWebsite(WEBSITE1);
+            Assert.Greater(IPMonitor.websites.Count, 0);
+            IPMonitor.RemoveWebsite(WEBSITE1);
+            Assert.AreEqual(0, IPMonitor.websites.Count);
+        }
+        [Test]
+        public void ShouldAddDuplicateWebsite()
+        {
+            IPMonitor.AddWebsite(WEBSITE1);
+            IPMonitor.AddWebsite(WEBSITE1);
+            Assert.AreEqual(IPMonitor.websites.Count, 1);
+        }
+        [Test]
+        public void ShouldAddThreeWebsites()
+        {
+            IPMonitor.AddWebsite(WEBSITE1);
+            IPMonitor.AddWebsite(WEBSITE2);
+            IPMonitor.AddWebsite(WEBSITE3);
+            Assert.IsTrue(WebsitesAreNotEmpty(IPMonitor.websites));
+            Assert.AreEqual(IPMonitor.websites.Count, 3);
         }
         [Test]
         public void ShouldHaveConnectionTrue()
         {
 
-            IPMonitor.AddWebsite("http://icanhazip.com");
+            IPMonitor.AddWebsite(WEBSITE1);
             Assert.IsTrue(IPMonitor.IsConnectionActive());
         }
+        public bool WebsitesAreNotEmpty(WebsiteHashSet websites)
+        {
+            foreach (string website in websites)
+            {
+                if (string.IsNullOrEmpty(website))
+                    return false;
+            }
+            return true;
+        }
+
     }
 }
